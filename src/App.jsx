5aeb5c809b58ec37b18d297d60321a00c2fd165e
Maheshwari242008 <mahiraccha24@gmail.com>
2026-06-01 12:19:15 +0530

    updated css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  //const [count2, setCount2] = useState(0);
   function handleClick() {
     setCount(count + 1);
   }
@@ -26,4 +25,5 @@ function App() {
   </div>
   );
 }
+
 export default App;
